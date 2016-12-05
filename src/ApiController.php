a22edc5503e7235d522a84f95e0584e3d5be1424
pausermake <pausermake@126.com>
2016-12-05 17:29:19 +0800

    移除部分暂不需要的文件

--- src/ApiController.php
+++ src/ApiController.php
@@ -6,9 +6,33 @@ use think\Config;
 
 class ApiController
 {
-    public function run(){
-//        $ucenter_config =  Config::get("ucenter");
-        //$api = new Api();
+    use Util;
 
+    private $allow = [];
+
+    public function run(){
+        $config = Config::get("ucenter");
+        $this->allow = $config['ALLOW_ACTION'];
+
+        $code = input('code');
+
+        parse_str(self::authcode($code, 'DECODE', $config['UC_KEY']), $get);
+
+//        if(time() - $get['time'] > 60) {
+//            exit('Authracation has expiried');
+//        }
+        if(empty($get)) {
+            exit('Invalid Request');
+        }
+        include_once 'xml.class.php';
+        $p_input = file_get_contents('php://input');
+        $post = xml_unserialize($p_input);
+
+        if(in_array($get['action'],$this->allow)){
+            $api = new ApiUtil($config);
+            call_user_func_array([$api,$get['action']],[$get,$post]);
+        }else{
+            exit(-1);
+        }
     }
 }
